df9f11d079d4677fbb7d6ed3676ab33fb0f05c4c
AAVN\tphvu <tongvu.244@gmail.com>
2022-09-15 19:04:56 +0700

    [OTHER]-11-feat: create Test for TermTopic JavaEE

--- src/test/java/com/axonactive/agileterm/service/TermTopicServiceTest.java
+++ src/test/java/com/axonactive/agileterm/service/TermTopicServiceTest.java
@@ -5,10 +5,7 @@ import com.axonactive.agileterm.dao.impl.TermTopicDAOImpl;
 import com.axonactive.agileterm.entity.TermEntity;
 import com.axonactive.agileterm.entity.TopicEntity;
 import com.axonactive.agileterm.entity.UserEntity;
-import com.axonactive.agileterm.rest.client.model.Description;
-import com.axonactive.agileterm.rest.client.model.Term;
 import com.axonactive.agileterm.rest.client.model.TermTopic;
-import com.axonactive.agileterm.rest.client.model.Topic;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,12 +17,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TermTopicServiceTest {
 
     @InjectMocks
-    private TermTopicService termTopicDAOService;
+    private TermTopicService termTopicService;
 
     @Mock
     private TopicService topicService;
@@ -44,10 +42,8 @@ class TermTopicServiceTest {
     TopicEntity topicProcess = new TopicEntity(3, "Scrum", "#ffcb00");
 
     List<TermEntity> terms = new ArrayList<>();
-
     TermEntity newTermDOD = new TermEntity(1, "Dod", new ArrayList<>());
-    TermEntity newTermDailyScrum =
-
+    TermEntity newTermDailyScrum = new TermEntity(2, "Daily Scrum", new ArrayList<>());
 
     @BeforeEach
     void setup(){
@@ -58,29 +54,31 @@ class TermTopicServiceTest {
         terms.add(newTermDOD);
         terms.add(newTermDailyScrum);
 
-        termTopicDAOService.save(new TermTopic(1, 1));
-        termTopicDAOService.save(new TermTopic(1, 2));
-        termTopicDAOService.save(new TermTopic(1, 3));
+        termTopicService.save(new TermTopic(1, 1));
+        termTopicService.save(new TermTopic(1, 2));
+        termTopicService.save(new TermTopic(1, 3));
 
         UserEntity userEntity1 = new UserEntity();
         userEntity1.setUsername("Huy");
         userEntity1.setEmail("admin@axonactive.com.vn");
         userEntity1.setPassword("Aavn123!@#");
+//        userDAO.save(userEntity1);
 
-        userDAO.save(userEntity1);
         UserEntity userEntity2 = new UserEntity();
         userEntity2.setUsername("Quang");
         userEntity2.setEmail("admn@axonactive.com.vn");
         userEntity2.setPassword("Aavn123!@#");
-        userDAO.save(userEntity2);
+//        userDAO.save(userEntity2);
 
-        descriptionService.save(1, new Description("A set of conditions that software must meet in order to be accepted by a customer or stakeholder", "Huy"));
-
-        descriptionService.save(1, new Description("A set of conditions that software must meet in order to be accepted by a customer or stakeholder", "Huy"));
+//        descriptionService.save(1, new Description("A set of conditions that software must meet in order to be accepted by a customer or stakeholder", "Huy"));
+//        descriptionService.save(1, new Description("A set of conditions that software must meet in order to be accepted by a customer or stakeholder", "Huy"));
     }
 
     @Test
     void findTopicByTermName() {
+        when(termTopicDAOImpl.findTopicByTermName("Dod")).thenReturn(topics);
+
+        assertEquals(topics.size(), termTopicService.findTopicByTermName("Dod").size());
     }
 
     @Test
@@ -93,8 +91,8 @@ class TermTopicServiceTest {
 
     @Test
     void testFindTermByTopicId_shouldReturn1_whenInput1AndCheckSize() {
-//        assertEquals(1, termTopicDAOService.findTermByTopicId(1).size());
-//        System.out.println(topicService.findById(1));
+        when(termTopicDAOImpl.findTermByTopicId(1)).thenReturn(List.of(terms.get(0)));
+        assertEquals(List.of(terms.get(0)), termTopicService.findTermByTopicId(1));
     }
 
     @Test
